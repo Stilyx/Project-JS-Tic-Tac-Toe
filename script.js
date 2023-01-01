9797127e55bc90416a8be496634905f8e066e9b1
Daniel Nunes <dani.perfil.profissional@gmail.com>
2023-01-01 19:29:04 -0300

    Update
    Update

--- script.js
+++ script.js
@@ -1,37 +1,95 @@
 const menu = document.querySelector('.menu');
 const game = document.querySelector('.game');
-const result = document.querySelector('.popup');
-const gameStartTurn = document.querySelector('.game-action');
-const isXorCircle = document.getElementsByName('choosecircleandx');
-const buttonStart = document.querySelector('.cpOrPlayer');
-const back = document.querySelector('.return');
-const turn = document.querySelector('.turn');
-const box = document.querySelectorAll('.box');
-const whowin = document.querySelector('.whowin');
-const menuPopUp = document.querySelector('.quitreturn');
-const resultMessage = document.querySelector('.result');
-const ties = document.querySelector('.ties');
-const loses = document.querySelector('.loses');
-const wins = document.querySelector('.player-wins');
+const playerStart = document.getElementsByName('choosecircleandx');
+const buttons = document.querySelector('.cpOrPlayer');
 const playerX = document.querySelector('.player-x');
 const playerO = document.querySelector('.player-o');
+const gameBoard = document.querySelector('.game-action');
+const divbox = document.querySelectorAll('.box');
+const turn = document.querySelector('.turn');
+const resultPlayerWin = document.querySelector('.result');
+let wins = document.querySelector('.player-wins');
+let loses = document.querySelector('.loses');
+let ties = document.querySelector('.ties');
+const popUp = document.querySelector('.popup');
+const resultmessage = document.querySelector('.whowin');
+const restart = document.querySelector('.animation');
+const playAgain = document.querySelector('.quitreturn');
+const result = document.querySelector('.result');
 
-// Player Or CPU Button
+buttonStartGame();
+playerSelected();
 
-buttonStart.addEventListener('click', e => {
-  const isClassList = e.target.classList[0];
+function buttonStartGame() {
+  buttons.addEventListener('click', e => {
+    const isPlayerOrCPU = e.target.classList[0];
 
-  if (isClassList === 'cpu') {
-    console.log('Escolheu CPU');
-  } else if (isClassList === 'player') {
-    gameStartTurn.classList.add('x-turn');
-    playersSelected();
-    menu.style.display = 'none';
-    game.style.display = 'flex';
+    if (isPlayerOrCPU === 'cpu') {
+      menu.style.display = 'none';
+      game.style.display = 'flex';
+      gameBoard.classList.add('x-turn');
+    } else if (isPlayerOrCPU === 'player') {
+      menu.style.display = 'none';
+      game.style.display = 'flex';
+      gameBoard.classList.add('x-turn');
+      playerSelected();
+    }
+  });
+}
+
+function playerSelected() {
+  playerStart.forEach(option => {
+    if (option.checked) {
+      if (option.id === 'x_text') {
+        playerX.innerHTML = 'Player One (X)';
+        playerO.innerHTML = 'Player Two (O)';
+      } else {
+        playerO.innerHTML = 'Player One (O)';
+        playerX.innerHTML = 'Player Two (X)';
+      }
+    }
+  });
+}
+
+function clickBoard(e) {
+  if (gameBoard.classList[1] === 'x-turn') {
+    gameBoard.classList.remove('x-turn');
+    gameBoard.classList.add('o-turn');
+    e.target.classList.add('x');
+    turn.childNodes[1].src = 'images/options-O.svg';
+  } else {
+    gameBoard.classList.remove('o-turn');
+    gameBoard.classList.add('x-turn');
+    e.target.classList.add('o');
+    turn.childNodes[1].src = 'images/options-X.svg';
   }
+
+  const win = wingame(e.target.classList[1]);
+  const isDrawGame = isDraw();
+  if (win) {
+    scoreBoard();
+    gameResult();
+    markvalues(e.target.classList[1]);
+  } else if (isDrawGame) {
+    let isnumberofties = Number(ties.innerText);
+    popUp.style.display = 'flex';
+    resultmessage.childNodes[1].style.display = 'none';
+    resultmessage.childNodes[3].innerHTML = 'ROUND TIED';
+    resultmessage.childNodes[3].style.color = '#A8BFC9';
+    isnumberofties++;
+    ties.innerHTML = isnumberofties;
+  } else {
+    resultmessage.childNodes[1].style.display = 'flex';
+    resultmessage.childNodes[3].innerHTML = 'Takes The Round';
+  }
+}
+
+divbox.forEach(box => {
+  box.addEventListener('click', clickBoard, {once: true});
 });
 
-const winneroptions = [
+// Win conditions
+const winconditions = [
   [0, 1, 2],
   [3, 4, 5],
   [6, 7, 8],
@@ -42,130 +100,101 @@ const winneroptions = [
   [2, 4, 6]
 ];
 
-// Tic Tac Toe
-
-const handclick = e => {
-  const cell = e.target;
-  const cellturn = gameStartTurn.classList[1];
-  if (cellturn === 'x-turn') {
-    cell.classList.add('x');
-    gameStartTurn.classList.remove('x-turn');
-    gameStartTurn.classList.add('o-turn');
-    turn.childNodes[1].src = 'images/options-O.svg';
-  } else {
-    cell.classList.add('o');
-    gameStartTurn.classList.remove('o-turn');
-    gameStartTurn.classList.add('x-turn');
-    turn.childNodes[1].src = 'images/options-X.svg';
-  }
-
-  const isDrawGame = ifDraw();
-  if (isDrawGame) {
-    let isnumberofties = Number(ties.innerText);
-    result.style.display = 'flex';
-    resultMessage.innerHTML = '';
-    whowin.childNodes[1].style.display = 'none';
-    whowin.childNodes[3].innerHTML = 'ROUND TIED';
-    whowin.childNodes[3].style.color = '#A8BFC9';
-    isnumberofties++;
-    ties.innerHTML = isnumberofties;
-  }
-
-  checkForwin(e.target.classList[1]);
-};
-
-const checkForwin = currentClass => {
-  return winneroptions.some(combination => {
-    let check = combination.every(index => box[index].classList.contains(currentClass));
-    if (check) {
-      scoreboard();
-      scoreResult(combination);
-    }
-  });
-};
-
-// Winning message
-
-function scoreResult(combination) {
-  combination.forEach(idx => {
-    const wongame = gameStartTurn.classList[1];
-    if (wongame === 'o-turn') {
-      whowin.childNodes[1].src = 'images/X-result.png';
-      whowin.childNodes[3].style.color = '#31c3bd';
-      box[idx].classList.add('xwinner');
-    } else {
-      whowin.childNodes[1].src = 'images/O-result.png';
-      whowin.childNodes[3].style.color = '#f2b137';
-      box[idx].classList.add('owinner');
-    }
-    result.style.display = 'flex';
-  });
-}
-
-function scoreboard() {
-  const wongame = gameStartTurn.classList[1];
-
-  if (wongame === 'o-turn') {
-    let xwinsScore = Number(wins.innerText);
-    xwinsScore++;
-    wins.innerHTML = xwinsScore;
-  } else {
-    let owinsScore = Number(loses.innerText);
-    owinsScore++;
-    loses.innerHTML = owinsScore;
-  }
-}
-
-box.forEach(div => {
-  div.addEventListener('click', handclick, {once: true});
-});
-
-// Score Player Select
-
-function playersSelected() {
-  isXorCircle.forEach(radio => {
-    if (radio.checked) {
-      if (radio.id === 'x_text') {
-        playerX.innerHTML = 'P1';
-        playerO.innerHTML = 'P2';
-      } else {
-        playerX.innerHTML = 'P2';
-        playerO.innerHTML = 'P1';
-      }
-    }
-  });
-}
-
-// Restart OR Quit
-back.addEventListener('click', resetGame);
-function resetGame() {
-  window.location.reload();
-}
-
-menuPopUp.addEventListener('click', e => {
-  if (e.target.textContent === 'QUIT') {
-    resetGame();
-  } else {
-    box.forEach(div => {
-      div.classList.remove('o');
-      div.classList.remove('x');
-      div.classList.remove('xwinner');
-      div.classList.remove('owinner');
-      div.removeEventListener('click', handclick);
-      div.addEventListener('click', handclick, {once: true});
+// Win game
+const wingame = currentPlayer => {
+  return winconditions.some(combination => {
+    return combination.every(index => {
+      return divbox[index].classList.contains(currentPlayer);
     });
-    result.style.display = 'none';
-    gameStartTurn.classList.remove('o-turn');
-    gameStartTurn.classList.remove('x-turn');
-    gameStartTurn.classList.add('x-turn');
+  });
+};
+
+// Mark Who winners
+
+const markvalues = currentPlayer => {
+  winconditions.forEach(function (combination) {
+    let check = combination.every(index => divbox[index].classList.contains(currentPlayer));
+    if (check) {
+      markWin(combination);
+    }
+  });
+};
+
+function markWin(combination) {
+  combination.forEach(function (index) {
+    if (gameBoard.classList[1] === 'o-turn') {
+      divbox[index].classList.add('xwinner');
+    } else {
+      divbox[index].classList.add('owinner');
+    }
+  });
+}
+
+// Score Board
+
+function scoreBoard() {
+  if (gameBoard.classList[1] === 'o-turn') {
+    let xscore = Number(wins.innerText);
+    xscore++;
+    wins.textContent = xscore;
+  } else {
+    let oscore = Number(loses.innerText);
+    oscore++;
+    loses.textContent = oscore;
+  }
+}
+
+// Game result PopUp
+
+function gameResult() {
+  const text = resultmessage.childNodes[3];
+  const imgChange = resultmessage.childNodes[1];
+  const isOTurn = gameBoard.classList[1] === 'o-turn';
+
+  if (isOTurn) {
+    text.innerText = 'Takes the Round';
+    text.style.color = '#31c3bd';
+    imgChange.src = 'images/X-result.png';
+  } else {
+    text.innerText = 'Takes the Round';
+    text.style.color = '#f2b137';
+    imgChange.src = 'images/O-result.png';
+  }
+
+  popUp.style.display = 'flex';
+}
+
+// Restart ou Quit Game
+
+restart.addEventListener('click', e => {
+  window.location.reload();
+});
+
+playAgain.addEventListener('click', e => {
+  const option = e.target.textContent;
+  if (option === 'NEXT ROUND') {
+    divbox.forEach(box => {
+      box.classList.remove('x');
+      box.classList.remove('o');
+      box.classList.remove('xwinner');
+      box.classList.remove('owinner');
+      box.removeEventListener('click', clickBoard, {once: true});
+      box.addEventListener('click', clickBoard, {once: true});
+    });
+    popUp.style.display = 'none';
+    gameBoard.classList.remove('o-turn');
+    gameBoard.classList.remove('x-turn');
+    gameBoard.classList.add('x-turn');
     turn.childNodes[1].src = 'images/options-X.svg';
+  } else if (option === 'QUIT') {
+    window.location.reload();
   }
 });
 
-// If Draw The Game
+// Draw Game
 
-function ifDraw() {
-  return [...box].every(div => {
-    return div.classList.contains('x') || div.classList.contains('o');
+const isDraw = () => {
+  return [...divbox].every(box => {
+    return box.classList.contains('x') || box.classList.contains('o');
   });
-}
+};
